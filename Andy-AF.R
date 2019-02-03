17c0deab0df33ee9b54b49dd69f2b982758e2641
andyhaixinchen <47197124+andyhaixinchen@users.noreply.github.com>
2019-02-03 13:23:59 -0800

    Add files via upload

--- Andy-AF.R
+++ Andy-AF.R
@@ -1,149 +1,164 @@
 library(bigrquery)
 proj_id <- 'hult-r-project'
 sql <- "SELECT * FROM `air_france.AF`"
-tb <- bq_project_query(proj_id, sql)
-google_AF <- bq_table_download(tb)
-AF <- data.frame(google_AF)
+#tb <- 
+AF <- bq_table_download(bq_project_query(proj_id, sql))
+#AF <- data.frame(AF)
 summary(AF)
-
-
-# Counting unique values in columns:
-
-for (i in 1:ncol(AF)){
-  if (is.character(AF[,i])){
-    print(summary(as.factor(AF[,i])))
-  }
-}
-
-#summary(as.factor(AF$Keyword))
-#summary(as.factor(AF$Campaign)) # 1214 unassigned
-#summary(as.factor(AF$Category)) # 1214 unassigned
-#This column contain values of destinations, 'airflight gernal', 'discountr', and 'vaccation'.
-#It does not seem to be valuable because of the quality of record. 
-#summary(as.factor(AF$Keyword_Group)) # 1214 unassigned
-#summary(as.factor(AF$Keyword_Type)) # Useless. This is columns has only one value "unassigned".
-#summary(as.factor(AF$Keyword_ID)) # Unique IDs for each observation
-#summary(as.factor(AF$Match_Type)) # 48 N/A. What is it?
-#summary(as.factor(AF$Bid_Strategy)) # 1224 NA's. What is it?
-#summary(as.factor(AF$Status)) # 1120 Unavalable 
-#unique(AF$Publisher_ID) # 7 IDs
-#unique(AF$Publisher_Name) # 7 names
-
-
-
-
-# Impute missing values with average. But there is no missing values.
-for( v in ncol(AF)){
-  if( mode(AF$v) == 'numeric') {
-    for(i in 1:nrow(AF)){
-      AF$v[which(is.na(AF$v))] <- mean(AF$v[which(!is.na(AF$v))])
-      return(AF)
-    }
-  }
-}
-
-AF[which(is.na(AF$Amount)), ] # Result: there are no missing values in numeric columes. 
-
-
+AF$Profit <- (AF$Amount - AF$Total_Cost)
+AF$ROI <- (AF$Profit / AF$Total_Cost)
+AF$ROA <- (AF$Amount / AF$Total_Cost)
 
 # Subsets 1: By Publisher, Global and US
 
 library(sqldf)
 
-publisher_performance <- sqldf("SELECT Publisher_Name, 
-                                 ROUND(SUM(Amount)/SUM(Total_Cost),3) AS ROA
-                                 FROM AF
-                                 GROUP BY Publisher_Name
-                                 ORDER BY ROUND(SUM(Amount)/SUM(Total_Cost),3) DESC;
-                                ")
-Publisher_KPI <- sqldf("
-                       SELECT Publisher_Name,
-                       ROUND(Sum(Impressions),3) AS Total_Impression,
-                       ROUND(AVG(Engine_Click_Thru__),3) AS Avg_Click_Through,
-                       ROUND(AVG(Trans__Conv___),3) AS Avg_Trans_Conv__,                      
-                       ROUND(SUM(Amount)/SUM(Total_Cost),3) AS ROA
+Publisher_Performance <- sqldf("
+                       SELECT
+                       CASE WHEN Publisher_Name LIKE '%Google%' THEN 'Google'
+                       WHEN Publisher_Name LIKE '%MSN%' THEN 'MSN'
+                       WHEN Publisher_Name LIKE '%Overture%' THEN 'Overture'
+                       WHEN Publisher_Name LIKE '%Yahoo%' THEN 'Yahoo'
+                       ELSE 'Not defined'
+                       END AS Publisher,
+                       Sum(Profit) AS sum_profit, 
+                       SUM(Amount)/SUM(Total_Cost) AS ROA
                        FROM AF
-                       GROUP BY Publisher_Name;
+                       GROUP BY Publisher
+                       ORDER BY sum(Profit) DESC;
                        ")
-library(minpack.lm)
-max_impression <- max(Publisher_KPI$Total_Impression)
-max_click_through <- max(Publisher_KPI$Avg_Click_Through)
-max_trans_con <- max(Publisher_KPI$Avg_Trans_Conv__)
-max_roa <- max(Publisher_KPI$ROA)
+Scale_Performance <- sqldf("
+                               SELECT
+                               CASE WHEN Publisher_Name LIKE '%Global%' THEN 'Global'
+                               WHEN Publisher_Name LIKE '%US%' THEN 'US'
+                               ELSE 'Not defined'
+                               END AS Scale,
+                               Sum(Profit) AS sum_profit, 
+                               SUM(Amount)/SUM(Total_Cost) AS ROA
+                               FROM AF
+                               GROUP BY Scale
+                               ORDER BY sum(Profit) DESC;
+                               ")
+Global_Performance <- sqldf("
+                               SELECT Publisher_Name,
+                               Sum(Profit) AS sum_profit, 
+                               SUM(Amount)/SUM(Total_Cost) AS ROA
+                               FROM AF
+                               WHERE Publisher_Name LIKE '%Global%'
+                               GROUP BY Publisher_Name
+                               ORDER BY sum(Profit) DESC;
+                               ")
+US_Performance <- sqldf("
+                            SELECT Publisher_Name,
+                            Sum(Profit) AS sum_profit, 
+                            SUM(Amount)/SUM(Total_Cost) AS ROA
+                            FROM AF
+                            WHERE Publisher_Name LIKE '%US%'
+                            GROUP BY Publisher_Name
+                            ORDER BY sum(Profit) DESC;
+                            ")
+############################################################################################
+Publisher_KPI <- sqldf("
+                              SELECT
+                               CASE WHEN Publisher_Name LIKE '%Google%' THEN 'Google'
+                               WHEN Publisher_Name LIKE '%MSN%' THEN 'MSN'
+                               WHEN Publisher_Name LIKE '%Overture%' THEN 'Overture'
+                               WHEN Publisher_Name LIKE '%Yahoo%' THEN 'Yahoo'
+                               ELSE 'Not defined'
+                               END AS Publisher,
+                               SUM(Clicks) AS sum_clicks,
+                               SUM(Total_Cost) AS sum_cost,
+                               SUM(Total_Volume_of_Bookings) AS sum_bookings,
+                               SUM(Amount) AS sum_revenue,
+                               (SUM(Amount) - SUM(Total_Cost)) AS sum_profit,
+                               SUM(Amount)/SUM(Total_Cost) AS ROA
+                               FROM AF
+                               GROUP BY Publisher
+                               ORDER BY sum(Profit) DESC;
+                               ")
+Kayak_clicks <- 2839
+kayak_media_cost <- 3567.13
+Kayak_total_bookings <- 208
+kayak_total_revenue <- 233694.00
+Kayak_profit <- 230126.87
+Kayak_ROA <- 65.51
+Kayak_obj <- c(Kayak_clicks, kayak_media_cost, Kayak_total_bookings, kayak_total_revenue, Kayak_profit, Kayak_ROA)
 
-MD_obj <- c(max_impression, max_click_through, max_trans_con, max_roa)
-GG <- c(1808326, 8.994, 0.433, 7.686) 
-GU <- c(3855689, 15.090, 0.415, 4.936)
-MG <- c(139979, 7.336, 1.133, 11.967)
-MU <- c(170120, 8.264, 0.731, 11.277)
-OG <- c(17898727, 3.548, 0.236, 6.689)
-OU <- c(17062488, 2.673, 0.095, 2.447)
-YH <- c(933345, 16.059, 1.829, 19.098)
-my_func <- function(b1, b2, b3, b4, b5, b6, b7){
-  obj <- b1*GG + b2*GU + b3*MG + b4*MU + b5*OG + b6*OU + b7*YH
+MD_obj <- 1.5* c(mean(Publisher_KPI[,2]),mean(Publisher_KPI[,3]),mean(Publisher_KPI[,4]),mean(Publisher_KPI[,5]),mean(Publisher_KPI[,6]),mean(Publisher_KPI[,7]))
+Google <- c(Publisher_KPI[1,2], Publisher_KPI[1,3], Publisher_KPI[1,4], Publisher_KPI[1,5], Publisher_KPI[1,6], Publisher_KPI[1,7]) 
+Yahoo <- c(Publisher_KPI[2,2], Publisher_KPI[2,3], Publisher_KPI[2,4], Publisher_KPI[2,5], Publisher_KPI[2,6], Publisher_KPI[2,7])
+Overture <- c(Publisher_KPI[3,2], Publisher_KPI[3,3], Publisher_KPI[3,4], Publisher_KPI[3,5], Publisher_KPI[3,6], Publisher_KPI[3,7])
+MSN <- c(Publisher_KPI[4,2], Publisher_KPI[4,3], Publisher_KPI[4,4], Publisher_KPI[4,5], Publisher_KPI[4,6], Publisher_KPI[4,7])
+
+############################################################################
+
+Publisher_KPI <- sqldf("
+                              SELECT
+                               CASE WHEN Publisher_Name LIKE '%Google%' THEN 'Google'
+                               WHEN Publisher_Name LIKE '%MSN%' THEN 'MSN'
+                               WHEN Publisher_Name LIKE '%Overture%' THEN 'Overture'
+                               WHEN Publisher_Name LIKE '%Yahoo%' THEN 'Yahoo'
+                               ELSE 'Not defined'
+                               END AS Publisher,
+                               SUM(Clicks) AS sum_clicks,
+                               AVG(Engine_Click_Thru__) AS click_through_rate,
+                               AVG(Trans__Conv___) AS conversion_rate,
+                               SUM(Impressions) AS impression,
+                               AVG(Avg__Pos_) AS average_position,
+                               AVG(Total_Cost__Trans_) AS cost_per_conversion,
+                               SUM(Total_Cost) AS sum_cost,
+                               SUM(Total_Volume_of_Bookings) AS sum_bookings,
+                               SUM(Amount) AS sum_revenue,
+                               SUM(Amount)/SUM(Total_Cost) AS ROA
+                               FROM AF
+                               WHERE Profit > 0
+                               GROUP BY Publisher
+                               ")
+MD_obj <- c(1.5*mean(Publisher_KPI[,2]),
+            1.5*mean(Publisher_KPI[,3]),
+            1.5*mean(Publisher_KPI[,4]),
+            1.5*mean(Publisher_KPI[,5]),
+            1.5*mean(Publisher_KPI[,6]),
+            1.5*mean(Publisher_KPI[,7]),
+            0.75*mean(Publisher_KPI[,8]),
+            1.5*mean(Publisher_KPI[,9]),
+            1.5*mean(Publisher_KPI[,10]),
+            1.5*mean(Publisher_KPI[,11]))
+Google <- c(Publisher_KPI[1,2], Publisher_KPI[1,3], Publisher_KPI[1,4], Publisher_KPI[1,5], Publisher_KPI[1,6], Publisher_KPI[1,7], Publisher_KPI[1,8], Publisher_KPI[1,9], Publisher_KPI[1,10], Publisher_KPI[1,11]) 
+Yahoo <- c(Publisher_KPI[2,2], Publisher_KPI[2,3], Publisher_KPI[2,4], Publisher_KPI[2,5], Publisher_KPI[2,6], Publisher_KPI[2,7], Publisher_KPI[2,8], Publisher_KPI[2,9], Publisher_KPI[2,10], Publisher_KPI[2,11])
+Overture <- c(Publisher_KPI[3,2], Publisher_KPI[3,3], Publisher_KPI[3,4], Publisher_KPI[3,5], Publisher_KPI[3,6], Publisher_KPI[3,7], Publisher_KPI[3,8], Publisher_KPI[3,9], Publisher_KPI[3,10], Publisher_KPI[3,11])
+MSN <- c(Publisher_KPI[4,2], Publisher_KPI[4,3], Publisher_KPI[4,4], Publisher_KPI[4,5], Publisher_KPI[4,6], Publisher_KPI[4,7], Publisher_KPI[4,8], Publisher_KPI[4,9], Publisher_KPI[4,10], Publisher_KPI[4,11])
+
+################## Optimization Model ##############
+library(minpack.lm)
+my_func <- function(b1, b2, b3, b4){
+  obj <- b1*Google + b2*Yahoo + b3*Overture + b4*MSN
   return(as.numeric(obj))
 }
 
-
-KPI_model <- nlsLM(MD_obj ~ my_func(b1, b2, b3, b4, b5, b6, b7))
+KPI_model <- nlsLM(MD_obj ~ my_func( b1, b2, b3, b4))
 summary(KPI_model)
 
 
-################################ Optimization Model for US Publishers ####################################################
-Publisher_KPI_Global <- sqldf("
-                       SELECT Publisher_Name,
-                       ROUND(Sum(Impressions),3) AS Total_Impression,
-                       ROUND(AVG(Engine_Click_Thru__),3) AS Avg_Click_Through,
-                       ROUND(AVG(Trans__Conv___),3) AS Avg_Trans_Conv__,                      
-                       ROUND(SUM(Amount)/SUM(Total_Cost),3) AS ROA
-                       FROM AF
-                       WHERE Publisher_Name LIKE '%Global%'
-                       GROUP BY Publisher_Name;
-                       ")
-
-max_impression_g <- max(Publisher_KPI_Global$Total_Impression)
-max_click_through_g <- max(Publisher_KPI_Global$Avg_Click_Through)
-max_trans_con_g <- max(Publisher_KPI_Global$Avg_Trans_Conv__)
-max_roa_g <- max(Publisher_KPI_Global$ROA)
-
-MD_obj_g <- c(max_impression_g, max_click_through_g, max_trans_con_g, max_roa_g)
-
-my_func_g <- function(b1, b2, b3){
-  obj <- b1*GG + b2*MG + b3*OG
-  return(as.numeric(obj))
-}
-
-KPI_model_g <- nlsLM(MD_obj_g ~ my_func_g(b1, b2, b3))
-summary(KPI_model_g)
 
 
-######################### Optimization model for US Publishers #######################################
-Publisher_KPI_US <- sqldf("
-                              SELECT Publisher_Name,
-                              ROUND(Sum(Impressions),3) AS Total_Impression,
-                              ROUND(AVG(Engine_Click_Thru__),3) AS Avg_Click_Through,
-                              ROUND(AVG(Trans__Conv___),3) AS Avg_Trans_Conv__,                      
-                              ROUND(SUM(Amount)/SUM(Total_Cost),3) AS ROA
-                              FROM AF
-                              WHERE Publisher_Name LIKE '%US%'
-                              GROUP BY Publisher_Name;
-                              ")
-
-max_impression_us <- max(Publisher_KPI_US$Total_Impression)
-max_click_through_us <- max(Publisher_KPI_US$Avg_Click_Through)
-max_trans_con_us <- max(Publisher_KPI_US$Avg_Trans_Conv__)
-max_roa_us <- max(Publisher_KPI_US$ROA)
-
-MD_obj_us <- c(max_impression_us, max_click_through_us, max_trans_con_us, max_roa_us)
-
-my_func_us <- function(b1, b2, b3, b4){
-  obj <- b1*GU + b2*MU + b3*OU + b4*YH
-  return(as.numeric(obj))
-}
-
-KPI_model_us <- nlsLM(MD_obj_us ~ my_func_us(b1, b2, b3, b4))
-summary(KPI_model_us)
 
 
-# Subset 2: By campaigns in each publisher
+
+
+############################################################################
+
+library(ggplot2)
+library(plotly)
+# ggplot(data=AF, aes(Profit, ROA))+geom_point(aes(color = Publisher_Name)) # not linear relationship
+ggplot(data=AF, aes(Amount, ROI))+geom_point(aes(color = Publisher_Name)) # not linear relationship
+ggplot(data=AF, aes(Profit, ROI))+geom_point(aes(color = Publisher_Name)) # not linear relationship
+ggplot(data=AF, aes(Amount, ROA))+geom_point(aes(color = Publisher_Name)) # not linear relationship
+
+ggplot(data=AF, aes(Total_Volume_of_Bookings, Amount))+geom_point(aes(color = Publisher_Name)) #linear relationship
+ggplot(data=AF, aes(Total_Volume_of_Bookings, Profit))+geom_point(aes(color = Publisher_Name)) #linear relationship 
+ggplot(data=AF, aes(Amount, Profit))+geom_point(aes(color = Publisher_Name)) #linear relationship 
+ggplot(data=AF, aes(ROA, ROI))+geom_point(aes(color = Publisher_Name)) # linear relationship
+plot_ly(Publisher_Performance, x = ~ROA, y = ~ROI,  type="scatter", mode = "markers" , color = ~Publisher)
+plot_ly(Publisher_Performance, x = ~sum_bookings, y = ~sum_profit,  type="scatter", mode = "markers" , color = ~Publisher)
 
